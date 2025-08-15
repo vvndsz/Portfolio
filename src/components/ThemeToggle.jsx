35207e8e88486b2fc801836fe98bc3e4a5319c16
vvndsz <vivien8534@gmail.com>
2025-08-15 15:15:49 +0530

    Mode 3

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -28,19 +28,19 @@ export const ThemeToggle = () => {
     }
   };
 
-  // return (
-  //   <button
-  //     onClick={toggleTheme}
-  //     className={cn(
-  //       "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-  //       "focus:outlin-hidden"
-  //     )}
-  //   >
-  //     {isDarkMode ? (
-  //       <Sun className="h-6 w-6 text-yellow-300" />
-  //     ) : (
-  //       <Moon className="h-6 w-6 text-blue-900" />
-  //     )}
-  //   </button>
-  // );
+  return (
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outlin-hidden"
+      )}
+    >
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
+    </button>
+  );
 };
